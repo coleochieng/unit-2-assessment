ebf8b294782fec5f304e8fbe9452d0dad9fc984c
Nicole Ochieng <jobochieng@jobs-mbp.home>
2022-08-02 17:52:40 -0500

    finish index routes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # unit-2-assessment
+
+to see in browser via localhost: localhost:3000
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const todos = require('../data/todos');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { todos });
+});
+
+
+router.post('/todos', function(req, res) {
+  // req.body.fullTank = true;
+  todos.push(req.body);
+  console.log(todos);
+  res.redirect('/');
+});
+
+router.delete('/todos/:idx', function(req, res) {
+  todos.splice(req.params.idx, 1);
+  res.redirect('/');
 });
 
 module.exports = router;
